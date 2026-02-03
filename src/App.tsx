4e12ee0d5caf827e51e188e6ae4270a2b838ed76
Jane Romero <20241392@s.ubaguio.edu>
2026-02-03 12:40:44 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Header from './Header'
+import Student from './Student'
+import Footer from './Footer'
+import Button from './Button'
+import './App.css'
+
+function App() {
+
+  const handleSave = () => alert("Saved!");
+  const handleCancel = () => alert("Cancelled!");
+
+  return (
+    <>
+     <Header />
+      <Student name="Jane Rose Romero" course="BSIT-1A" />
+      <Student name="Ryza Francisco" course="BSCS-2B" />
+      <Button label="Save" onClick={handleSave} />
+      <Button label="Cancel" onClick={handleCancel} />
+      <Footer />
+    </>
+  )
+}
+
+export default App
