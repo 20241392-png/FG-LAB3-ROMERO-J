b6527da408821c51584154b39e36aa652449690c
Jane Romero <20241392@s.ubaguio.edu>
2026-02-03 13:25:47 +0800

    Initial React project

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/FG-LAB3_ROMERO_J/',
+  base:'/FG-LAB3-ROMERO-J/',
 })
